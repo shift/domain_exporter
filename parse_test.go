109d95310f7431a121cfb49daf0d90517fe8a4a0
Vojtěch Káně <vojtech.kane@gmail.com>
2021-05-16 23:21:07 +0200

    Add test for .cz

--- parse_test.go
+++ parse_test.go
@@ -27,6 +27,7 @@ func TestParsing(t *testing.T) {
 		date     time.Time
 	}{
 		{filename: "google.cn", date: time.Date(2019, 3, 17, 12, 48, 36, 0, time.UTC)},
+		{filename: "google.cz", date: time.Date(2021, 7, 22, 0, 0, 0, 0, time.UTC)},
 		{filename: "google.jp", date: time.Date(2021, 5, 31, 0, 0, 0, 0, time.UTC)},
 		{filename: "google.com", date: time.Date(2020, 9, 14, 4, 0, 0, 0, time.UTC)},
 		{filename: "ietf.org", date: time.Date(2020, 3, 12, 5, 0, 0, 0, time.UTC)},
