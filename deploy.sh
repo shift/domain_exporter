af88aa35a89cea84cc983093279e53634b9c8e63
Vincent Palmer <shift@someone.section.me>
2022-04-08 21:16:48 +0200

    Removed some older non-working, non-required or unused files The start of a prometheus / grafana mixin based off the ones found in the node_exporter

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export GO_VERSION=$(go version | awk '{print $3}')
-echo "$REG_PW" | docker login quay.io --username "$REG_USER" --password-stdin
-curl -sL https://git.io/goreleaser | bash
